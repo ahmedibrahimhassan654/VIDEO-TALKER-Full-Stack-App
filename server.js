0213ec7db62e993c43be99ff795d634775ac70b8
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2021-06-22 00:14:31 +0200

    initiate server back end

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const socket = require('socket.io')
+
+
+const PORT = 5000
+
+
+const app = express()
+
+
+const server=app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+  console.log(`http://localhost:${PORT}`);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: '*',
+    methods:['GET','POST']
+  }
+})
+
+
+io.on('connection', (socket) =>
+{
+  socket.emit('connection', null)
+  console.log('new user connected');
+  console.log(socket.io);
+})
